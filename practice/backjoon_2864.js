7f44d1db7d0f4ca95a460887c3ddd6730372d311
이상우 <ev15963@hs.ac.kr>
2023-04-02 12:47:40 +0000

    5와 6의 차이 해결

--- practice/backjoon_2864.js
+++ practice/backjoon_2864.js
@@ -13,3 +13,50 @@
 // 첫째 줄에 상근이가 구할 수 있는 두 수의 합 중 최솟값과 최댓값을 출력한다.
 // 36 37
 
+let list = [11, 25];
+let new_list ='';
+let sum_new_list1 = 0;
+let sum_new_list2 = 0;
+let output = [];
+
+function solution(list) {
+	console.log(list);
+	let dd = JSON.stringify(list);
+	for (let d of dd) {
+		console.log(d);
+		if(d=='5') {
+			d='6';
+			console.log(d, 'if in');
+		} else if(d=='6') {
+			d='5';
+		}
+		new_list+=d;
+		console.log(new_list, 'new_list');
+
+	}
+	console.log(new_list, 'dd');
+	let new_list1 = JSON.parse(new_list);
+	console.log(new_list1);
+	
+	for (let i =0; i < list.length; i++) {
+		sum_new_list1+=list[i];
+	}
+	console.log(sum_new_list1, 'sum_new_list1');
+	
+	for (let i =0; i < new_list1.length; i++) {
+		sum_new_list2+=new_list1[i];
+	}
+	console.log(sum_new_list2, 'sum_new_list2');
+	
+	if (sum_new_list1 < sum_new_list2) {
+		output = [sum_new_list1, sum_new_list2];
+	} else {
+		output = [sum_new_list2, sum_new_list1];
+	}
+	console.log(output);
+	return output;
+}
+
+const sol = solution(list);
+
+console.log(sol);
