e3c7fd9d212be9463f1fa779d3cb73e7fb8d31c3
이상우 <ev15963@hs.ac.kr>
2023-04-16 07:49:04 +0000

    fifo stack test

--- thisiscoding/fifo.py
+++ thisiscoding/fifo.py
@@ -9,3 +9,16 @@ print(queue) # deque([1,2,3])
 
 print(queue.pop()) # 3
 print(queue) # deque([1,2])
+
+print('-----------------------');
+
+stack = []
+    
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print(stack) # [1,2,3]
+
+print(stack.pop()) # 3
+print(stack)  # [1,2]
