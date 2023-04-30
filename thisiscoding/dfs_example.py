16597dfe9a59134f4abfeb155155d51599e9debb
이상우 <ev15963@hs.ac.kr>
2023-04-30 07:51:44 +0000

    dfs_example init

--- thisiscoding/dfs_example.py
+++ thisiscoding/dfs_example.py
@@ -1,26 +1,6 @@
 # from typing import list
 from typing import List
 
-def dfs(
-    node: int,
-    visited_info: List[bool],
-    stack: List[int],
-    graph: List[List[int]],
-):
-    visited_info[node] = True
-    stack.append(node)
-
-    adjacent_nodes: List[int] = graph[node]
-    for adjacent_node in adjacent_nodes:
-        if not visited_info[adjacent_node]:
-            dfs(
-                node=adjacent_node,
-                visited_info=visited_info,
-                stack=stack,
-                graph=graph,
-            )
-
-
 graph: List[List[int]] = [
     [],
     [2, 3, 8],
@@ -38,3 +18,4 @@ visited_info: List[bool] = [False] * 9
 dfs(node=1, visited_info=visited_info, stack=stack, graph=graph)
 
 print(stack) # > [1, 2, 7, 6, 8, 3, 4, 5]
+
