ccb9da73b0ef249e1354d3e313c1ea9dda013d3b
이상우 <ev15963@hs.ac.kr>
2023-07-25 09:53:09 +0000

    이것이 코딩이다 DFS BFS

--- thisiscoding/bfs_example.py
+++ thisiscoding/bfs_example.py
@@ -8,11 +8,19 @@ def bfs(
     result: List[int],
     graph: List[List[int]],
 ) -> None:
+    print(node, "node");
+    print(visited_info[node], "visited_info[node] before");
     visited_info[node] = True
+    print(visited_info[node], "visited_info[node] after");
     while queue:
+        print(queue, "queue");
         current_node: int = queue.popleft()
+        print(current_node, "current_node");
         result.append(current_node)
+        print(result, "result");
+        print(graph[current_node], "graph[current_node]");
         for adjacent_node in graph[current_node]:
+            print(adjacent_node, "adjacent_node");
             if not visited_info[adjacent_node]:
                 queue.append(adjacent_node)
                 visited_info[adjacent_node] = True
--- thisiscoding/dfs_example.py
+++ thisiscoding/dfs_example.py
@@ -15,7 +15,33 @@ graph: List[List[int]] = [
 
 stack: List[int] = []
 visited_info: List[bool] = [False] * 9
-dfs(node=1, visited_info=visited_info, stack=stack, graph=graph)
 
 print(stack) # > [1, 2, 7, 6, 8, 3, 4, 5]
 
+def dfs(
+    node: int,
+    visited_info: List[bool],
+    stack: List[int],
+    graph: List[List[int]],
+):
+    print(visited_info, "visited_info");
+    visited_info[node] = True
+    print(stack, "stack");
+    stack.append(node)
+    print(stack, "stack");
+
+    adjacent_nodes: List[int] = graph[node]
+    print(adjacent_nodes, "adjacent_nodes");
+    for adjacent_node in adjacent_nodes:
+        print(adjacent_node, "adjacent_node");
+        if not visited_info[adjacent_node]:
+            print(visited_info[adjacent_node], "visited_info[adjacent_node]")
+            dfs(
+                node=adjacent_node,
+                visited_info=visited_info,
+                stack=stack,
+                graph=graph,
+            )
+            
+
+dfs(node=1, visited_info=visited_info, stack=stack, graph=graph)
