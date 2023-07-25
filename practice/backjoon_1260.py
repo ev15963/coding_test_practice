f00d902b40d6e426a07b95812ef196ccab71fe97
이상우 <ev15963@hs.ac.kr>
2023-07-25 11:22:39 +0000

    백준 dfs

--- practice/backjoon_1260.py
+++ practice/backjoon_1260.py
@@ -3,6 +3,19 @@ from __future__ import print_function
 
 import sys
 from collections import deque
+
+# input
+# 4 5 1
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
+
+# output
+# 1 2 4 3
+# 1 2 3 4
+
 input = sys.stdin.readline
 
 N, M, V = map(int,input().split())
