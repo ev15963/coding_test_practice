61a36feed4021f6beea342b67b0d07cdf65685c4
nicky <ev15963@hs.ac.kr>
2023-07-27 12:34:17 +0000

    알고리즘을 function화

--- programmers/147355.js
+++ programmers/147355.js
@@ -8,34 +8,28 @@
 // task 2 : t의 값을 p의 길이로 순서대로 나타내기 
 // task 2.1 : 문자열 다루기 // 문자열 트리밍 하기, 문자열 앞에서부터 잘라내기, 문자열 뒤에서 잘라내기, 문자열 중간에 잘라내기, 소문자-대문자 서로 변환하기, ..// 문자열 앞에서 부터 잘라내기
 // task 3 : 각각의 substring을 반복문으로 만들기
-
-// const t0 = t[0] + t[1] + t[2];
-// "500220839878"	"7"	8
-// const t = "314159211";
-// const p = "271";
-// const t = "500220839878";
-// const p = "7";
-
-// "10203"	"15"	3
 const t = "10203";
 const p = "15";
+function solution(t, p) {
+    const plength = p.length;
+    const list = []; //
 
-const plength = p.length;
-const list = []; //
-
-// const list = [314, 141, 415, 159, 592]; // 921, 211
-// 7의 길이를 게산
-for (let i = 0; i<t.length-(p.length-1); i++) {
-    let test = Number (t.substring(i, plength+i));
-    list.push(test);
-    console.log(list);
-}
-
-var num = 0;
-
-for (var i = 0; i < list.length; i++) {
-    if (Number(p) >= list[i]) {
-        num++;
+    // const list = [314, 141, 415, 159, 592]; // 921, 211
+    // 7의 길이를 게산
+    for (let i = 0; i<t.length-(p.length-1); i++) {
+        let test = Number (t.substring(i, plength+i));
+        list.push(test);
+        console.log(list);
     }
+
+    var num = 0;
+
+    for (let i = 0; i < list.length; i++) {
+        if (Number(p) >= list[i]) {
+            num++;
+        }
+    }
+    return num;
 }
-console.log(num);
+const ans = solution(t, p);
+console.log(ans);
