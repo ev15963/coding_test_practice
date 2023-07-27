4085689e1d4acbc647043e5e255d1ca670c20078
nicky <ev15963@hs.ac.kr>
2023-07-27 10:39:56 +0000

    t를 substring으로 출력하기

--- programmers/147355.js
+++ programmers/147355.js
@@ -7,13 +7,24 @@
 // task 1.1 : 기존 배열에서 반복문으로 변경
 // task 2 : t의 값을 p의 길이로 순서대로 나타내기 
 // task 2.1 : 문자열 다루기 // 문자열 트리밍 하기, 문자열 앞에서부터 잘라내기, 문자열 뒤에서 잘라내기, 문자열 중간에 잘라내기, 소문자-대문자 서로 변환하기, ..// 문자열 앞에서 부터 잘라내기
+// const t0 = t[0] + t[1] + t[2];
 
 const t = "3141592";
 const p = "271";
 
+const plength = p.length;
+
+const t0 = t.substring(0,plength)
+console.log(t0);
+// const t0 = "314";
+const t1 = t.substring(1, plength+1);
+console.log(t1);
+
+const t2 = t.substring(2, plength+2);
+console.log(t2);
 
 const n = Number(p);
-const list = [314, 141, 415, 159, 592];
+
 var num = 0;
 
 for (var i = 0; i < list.length; i++) {
--- programmers/176963.js
+++ programmers/176963.js
@@ -3,15 +3,19 @@
 // photo : [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
 // result : [19, 15, 6]
 
-
+/** 
+ * 배열.reduce((누적값, 현잿값, 인덱스, 요소) => { return 결과 }, 초깃값);
+ * acc : [ 'may', 'may', 'kon' ]
+ * cur : 
+*/
 function solution(name, yearning, photo) {
-    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)]))
+    return photo.map((v)=> v.reduce((acc, cur, i)=> cur))
 }
 
 let name = ["may", "kein", "kain", "radi"];
 let yearning = [5, 10, 1, 3];
 let photo = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]];
-let result = [19, 15, 6];
+// tobe : let result = [19, 15, 6];
 
 const ans = solution(name, yearning, photo);
 console.log(ans);
