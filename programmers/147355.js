8f9e955fd460dedac9c353c4d105adfb48851b5c
nicky <ev15963@hs.ac.kr>
2023-07-27 08:12:06 +0000

    task 1 : list에 있는 숫자가 작거나 같을 경우

--- programmers/147355.js
+++ programmers/147355.js
@@ -2,3 +2,29 @@
  * 크기가 작은 부분문자열
  * URL : https://school.programmers.co.kr/learn/courses/30/lessons/147355
  */
+
+// task 1 : list에 있는 숫자가 작거나 같을 경우
+
+const t = "3141592";
+const p = "271";
+const list = [314, 141, 415, 159, 592];
+
+const a = 1;
+const b = 3;
+const c = 0;
+
+const n = Number(p);
+var num = 0;
+
+console.log(a)
+if (n >= a) {
+    num++;
+}
+if (n >= b) {
+    num++
+}
+if (n >= c) {
+    num++
+}
+
+console.log(num);
