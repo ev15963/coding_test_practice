307d9338c55177923e98768e382566ca2a1f54aa
nicky <ev15963@hs.ac.kr>
2023-07-27 08:59:17 +0000

    task1.1 기본 배열에서 반복문으로 변경

--- programmers/147355.js
+++ programmers/147355.js
@@ -4,27 +4,17 @@
  */
 
 // task 1 : list에 있는 숫자가 작거나 같을 경우
-
+// task 1.1 : 기존 배열에서 반복문으로 변경
 const t = "3141592";
 const p = "271";
 const list = [314, 141, 415, 159, 592];
 
-const a = 1;
-const b = 3;
-const c = 0;
-
 const n = Number(p);
 var num = 0;
 
-console.log(a)
-if (n >= a) {
-    num++;
+for (var i = 0; i < list.length; i++) {
+    if (n >= list[i]) {
+        num++;
+    }
 }
-if (n >= b) {
-    num++
-}
-if (n >= c) {
-    num++
-}
-
 console.log(num);
