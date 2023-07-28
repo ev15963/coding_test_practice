c2388aeae21db7c014350ecf32ec631153b66b26
nicky <ev15963@hs.ac.kr>
2023-07-28 02:54:03 +0000

    reduce 처리

--- programmers/176963.js
+++ programmers/176963.js
@@ -12,9 +12,15 @@
  * 배열.reduce((누적값, 현잿값, 인덱스, 요소) => { return 결과 }, 초깃값);
  * acc : [ 'may', 'may', 'kon' ]
  * cur : 
+ * 참고 URL : https://www.zerocho.com/category/JavaScript/post/5acafb05f24445001b8d796d#google_vignette
+ * 참고 URL : https://school.programmers.co.kr/learn/courses/30/lessons/176963/solution_groups?language=javascript
+ * reduce 안 에서는 indexOf로 name에서 해당 사람의 인덱스를 가져온 후, 
+ * yearning에 데이터로 가져옮 
+ * 다만 점수 데이터가 없으면 undefined가 반환되므로 0으로 처리 
+ * 그걸 합산해서 반환
 */
 function solution(name, yearning, photo) {
-    return photo.map((v)=> v.reduce((acc, cur, i)=> cur))
+    return photo.map((v)=> v.reduce((acc, cur)=> acc += yearning[name.indexOf(cur)] ?? 0, 0));
 }
 
 let name = ["may", "kein", "kain", "radi"];
