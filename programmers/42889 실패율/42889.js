06e1404dd550aee8f7fc21dfb0b8235acfa5ec16
nicky <ev15963@hs.ac.kr>
2023-07-30 14:12:02 +0000

    42889 test code

--- programmers/42889 실패율/42889.js
+++ programmers/42889 실패율/42889.js
@@ -3,7 +3,14 @@
  * URL : https://school.programmers.co.kr/learn/courses/30/lessons/42889
  */
 
+const N = 5;
+const stages = [2, 1, 2, 6, 2, 4, 3, 3];
+
 function solution(N, stages) {
     var answer = [];
     return answer;
 }
+
+const ans = solution(N, stages);
+console.log(ans);
+// to be [3,4,2,1,5]
