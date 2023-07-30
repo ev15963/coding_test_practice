f8d531c4a2d64a7bce6825c4228e0d1dd8b4619d
nicky <ev15963@hs.ac.kr>
2023-07-30 13:40:14 +0000

    정규표현식 gi 적용

--- programmers/81301 숫자 문자열과 영단어/81301.js
+++ programmers/81301 숫자 문자열과 영단어/81301.js
@@ -14,13 +14,19 @@ function solution(s) {
     // Output: Hello world, JavaScript
     // URL : https://codechacha.com/ko/javascript-replace-in-string/
 
-    let num = s.replace('zero', 0).replace('one', 1).replace('two', 2).replace('three', 3).replace('four', 4).replace('five', 5).replace('six', 6).replace('seven', 7).replace('eight', 8).replace('nine', 9);
-    console.log(num); // 102zero3
+
+    // .replace(/seven/gi)
+    // replace 정규표현식 gi는 무엇을 의미할까?
+    // URL : https://webruden.tistory.com/980
+    // https://school.programmers.co.kr/learn/courses/30/lessons/81301/solution_groups?language=javascript
+
+    let num = s.replace(/zero/gi, 0).replace(/one/gi, 1).replace(/two/gi, 2).replace(/three/gi, 3).replace(/four/gi, 4).replace(/five/gi, 5).replace(/six/gi, 6).replace(/seven/gi, 7).replace(/eight/gi, 8).replace(/nine/gi, 9);
+    console.log(num); // 10203
     return Number(num);
 }
 
 const ans = solution(s);
 // to be : 1478
-console.log(ans); //NaN
+console.log(ans); //10203
 
 // 
