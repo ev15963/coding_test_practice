2e188da4915a29e783cd6ac7bcbeb3fcdcdf2b4e
nicky <ev15963@hs.ac.kr>
2023-07-30 12:58:40 +0000

    81301 test code

--- programmers/81301 숫자 문자열과 영단어/81301.js
+++ programmers/81301 숫자 문자열과 영단어/81301.js
@@ -3,7 +3,13 @@
  * URL : https://school.programmers.co.kr/learn/courses/30/lessons/81301
  */
 
+const s = "one4seveneight";
+
 function solution(s) {
     var answer = 0;
     return answer;
 }
+
+const ans = solution(s);
+// to be : 1478
+console.log(ans);
