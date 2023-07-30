0818791c8a2eba6b275d85eeb0d1c56d360229f0
nicky <ev15963@hs.ac.kr>
2023-07-30 12:08:57 +0000

    다른 테스트 값에 대응해서 완료

--- programmers/[1차] 비밀지도 17681/17681.js
+++ programmers/[1차] 비밀지도 17681/17681.js
@@ -8,25 +8,13 @@
 // task 1.2. 모두 공백인 부분은 전체 지도에서 공백
 // task 2. 벽 = 1, 공백 부분 = 0 부호화
 
-// 참고
-// 자바스크립트 2진수 변환 길이
-// let N = 7;
-// let padding = '00000000';
-// let result = N.toString(2).padStart(8, '0');
-// URL : https://chinsun9.github.io/2020/11/17/js-%EC%9D%B4%EC%A7%84%EC%88%98-%EC%95%9E%EC%97%90-0%EB%84%A3%EA%B8%B0/
-const n = 5;
-const arr1 = [9, 20, 28, 18, 11];
-const arr2 = [30, 1, 21, 17, 28];
+
+const n = 6;
+const arr1 = [46, 33, 33, 22, 31, 50];
+const arr2 = [27, 56, 19, 14, 14, 10];
 
 function solution(n, arr1, arr2) {
     console.log(n);
-    let a = arr1.map((v) => v.toString(2).padStart(5, '0'));
-    console.log(a);
-    
-    let b = arr2.map((v) => v.toString(2).padStart(5, '0'));
-    console.log(b);
-    
-    console.log(a[0][0]);
 
     // 
     // 자바스크립트 배열 끼리 값 더하기
@@ -44,17 +32,29 @@ function solution(n, arr1, arr2) {
 
     // - Array.prototype.map() 메서드 사용
     //   arrA.map((x, y) => x + arrB[y]); // [6, 8, 10, 12,]
-    let a_new = a.map((v,i)=> String(Number(v)+Number(b[i])));
-    console.log(a_new);
+    //let a_new = a.map((v,i)=> (v || (b[i])));
+    //console.log(a_new);
+
+    let arr_compare = arr1.map((v,i)=> (v | (arr2[i])));
+    console.log(arr_compare);
+
+    // 참고
+    // 자바스크립트 2진수 변환 길이
+    // let N = 7;
+    // let padding = '00000000';
+    // let result = N.toString(2).padStart(8, '0');
+    // URL : https://chinsun9.github.io/2020/11/17/js-%EC%9D%B4%EC%A7%84%EC%88%98-%EC%95%9E%EC%97%90-0%EB%84%A3%EA%B8%B0/
+    let decimal = arr_compare.map((v) => v.toString(2).padStart(n, '0'));
+    console.log(decimal);
 
     // 출력값으로 바꾸기
     //for (let i of a_new) {
     //    console.log(i);
     //}
-    let b_new = a_new.map((v)=> v.split('0').join(' ').split('1').join('#').split('2').join('#'));
-    console.log(b_new);
+    let arr_replace = decimal.map((v)=> v.split('0').join(' ').split('1').join('#'));
+    console.log(arr_replace);
 
-    return b_new;
+    return arr_replace;
 }
 const ans = solution(n, arr1, arr2);
 
