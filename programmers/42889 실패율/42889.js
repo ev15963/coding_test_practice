0fbbc8c3178eab3c4f6a6d013069cdba069dc167
nicky <ev15963@hs.ac.kr>
2023-07-31 03:01:56 +0000

    정렬 인덱스 추가, sort 내림차순

--- programmers/42889 실패율/42889.js
+++ programmers/42889 실패율/42889.js
@@ -14,13 +14,36 @@ function solution(N, stages) {
     for (let i=1; i<N+1; i++) {
         let fail_rate = stages.filter((v)=> v===i).length;
         let fail_rate2 = stages.filter((v)=> v>=i).length;
-        fail_arr.push(fail_rate / fail_rate2);
+        fail_arr.push([i, fail_rate / fail_rate2]);
         // console.log(fail_rate2);
     }
     console.log(fail_arr);
 
-    var answer = [];
-    return answer;
+    // task 2. 정렬
+    // URL : https://hianna.tistory.com/409
+    // https://carrotweb.tistory.com/185/
+    //    숫자 배열 정렬
+
+    // const number = [1, 5, 10, 15, 20, 25, 100];
+
+    // 오름차순 정렬(Ascending Order, ASC)
+    // number.sort(function(comp1, comp2) {
+    // 	return comp1 - comp2; 
+    // });
+
+    // console.log(number);
+    // --> [1, 5, 10, 15, 20, 25, 100]
+
+    // 내림차순 정렬(Descending Order, DESC)
+    // number.sort(function(comp1, comp2) {
+    // 	return comp2 - comp1; 
+    // });
+
+    // console.log(number);
+    // --> [100, 25, 20, 15, 10, 5, 1]
+    let fail_arr_sort = fail_arr.sort((a, b)=> b[1] - a[1]);
+    console.log(fail_arr_sort);
+    return fail_arr_sort.map((v)=> v[0]);
 }
 
 const ans = solution(N, stages);
