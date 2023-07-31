c454c3348aaea05e83fc3fd86d28cf88ee475a26
nicky <ev15963@hs.ac.kr>
2023-07-31 13:56:42 +0000

    빈 배열 만들기

--- Social_Infra_Tech/651 /651.js
+++ Social_Infra_Tech/651 /651.js
@@ -1,16 +1,28 @@
 const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
 function solution(matrix) {
     const resultMatrix = [];
 
-    for (let i = 0; i < matrix.length; i++) {
-    const row = [];
-    for (let j = 0; j < matrix[i].length; j++) {
-        row.push(matrix[i][j]);
-    }
-    resultMatrix.push(row);
-    }
-    console.log(resultMatrix)
+    // task 1. 빈 배열 만들기
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+
+    const rowSums = Array(numRows).fill(0);
+    const colSums = Array(numCols).fill(0);
+    
+    console.log(rowSums);
+    console.log(colSums);
+    
+    
+
+    // for (let i = 0; i < matrix.length; i++) {
+    // const row = [];
+    //     for (let j = 0; j < matrix[i].length; j++) {
+    //         row.push(matrix[i][j]);
+    //     }
+    //     resultMatrix.push(row);
+    // }
+    // console.log(resultMatrix)
 
     var answer = [[]];
 
