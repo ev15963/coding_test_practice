93dbb4635692ab1c514417f24a22754685bd10b3
nicky <ev15963@hs.ac.kr>
2023-07-31 00:18:35 +0000

    문제이해

--- programmers/42889 실패율/42889.js
+++ programmers/42889 실패율/42889.js
@@ -3,10 +3,14 @@
  * URL : https://school.programmers.co.kr/learn/courses/30/lessons/42889
  */
 
+// ans : https://school.programmers.co.kr/learn/courses/30/lessons/42889/solution_groups?language=javascript
+
 const N = 5;
 const stages = [2, 1, 2, 6, 2, 4, 3, 3];
-
+// task 1. stages의 길이의 사용자가  
 function solution(N, stages) {
+    console.log(stages);
+    
     var answer = [];
     return answer;
 }
