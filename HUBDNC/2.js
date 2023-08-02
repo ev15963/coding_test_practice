3dc4279b000e507b97728ed747d1616a045b0029
nicky <ev15963@hs.ac.kr>
2023-08-02 12:10:25 +0000

    최소공배수

--- HUBDNC/2.js
+++ HUBDNC/2.js
@@ -14,8 +14,32 @@ function solution(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             console.log(i, j);
+
         }
     }
+
+    // task 2. 최대공약수, 최소공배수
+    // 자바스크립트 최소공배수
+    // const solutuon = (n, m) => {
+    //     const gcd = (a, b) => {
+    //       if (b === 0) return a; // 나누어지면 a 리턴
+    //       return gcd(b, a % b); // 나누어지지 않는다면 b와 a%b를 다시 나눈다
+    //     };
+    //     const lcm = (a, b) => (a * b) / gcd(a, b); // 두 수의 곱을 최대공약수로 나눈다.
+    //     return console.log(
+    //       `최대 공약수는? ${gcd(n, m)}, 최대 공배수는? ${lcm(n, m)}`
+    //     );
+    //   };
+    //   console.log(solutuon(6, 12));
+    // 출처 URL : https://haerim95.tistory.com/36
+
+    // 최소공배수 gcd
+    const gcd = (a, b) => {
+        console.log(a, b);
+        if (b === 0) return a;
+        return gcd(b, a % b);
+    }
+    console.log(gcd(12, 8));
 
     return answer;
 }
