25e43e477f88f3024b679175d8b6deba5838b8ca
nicky <ev15963@hs.ac.kr>
2023-08-03 02:21:28 +0000

    O(log n)

--- HUBDNC/7.js
+++ HUBDNC/7.js
@@ -28,3 +28,23 @@ function another_O_n_algorithm(n) {
       // do something for 1 second
     }
 }
+
+//task 3. O(log n)
+function O_quadratic_algorithm(n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            // do something for 1 second
+        }
+    }
+}
+
+function another_O_quadratic_algorithm(n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            for (let k = 0; k < n; k++) {
+              // do something for 1 second
+            }
+        }
+    }
+}
+
