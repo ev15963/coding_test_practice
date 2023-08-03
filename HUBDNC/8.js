c43b404b417eb45ec785928dacaa6310d092b43f
nicky <ev15963@hs.ac.kr>
2023-08-03 13:59:59 +0000

    팩토리얼 함수 구하기

--- HUBDNC/8.js
+++ HUBDNC/8.js
@@ -4,3 +4,14 @@
 // 🚩 [제한사항]
 
 // - 모든 데이터는 null 값이 허용되지 않습니다.
+
+// task 1. 팩토리얼 함수 구하기
+const n = 5;
+
+function factorial(n) {
+    if (n == 1) return 1;
+    return n * factorial(n-1);
+}
+
+const ans = factorial(n);
+console.log(ans);
