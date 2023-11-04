f4b4cc4a603b4f68492b13074d86975c625fa238
nicky <ev15963@hs.ac.kr>
2023-11-04 12:25:36 +0000

    64062 solution init

--- programmers/64062 징검다리 건너기/64062.js
+++ programmers/64062 징검다리 건너기/64062.js
@@ -3,3 +3,10 @@
  * URL : https://school.programmers.co.kr/learn/courses/30/lessons/64062
  */
 
+ function solution(stones, k) {
+    var answer = 0;
+    return answer;
+}
+
+const ans = solution(stones, k);
+console.log(ans);
