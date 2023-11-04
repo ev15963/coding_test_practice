ce8e8f330a5fef438dd8e483c0d7b906fc42e3c5
nicky <ev15963@hs.ac.kr>
2023-11-04 12:35:04 +0000

    64062 설명 init

--- programmers/64062 징검다리 건너기/64062.js
+++ programmers/64062 징검다리 건너기/64062.js
@@ -3,10 +3,19 @@
  * URL : https://school.programmers.co.kr/learn/courses/30/lessons/64062
  */
 
+// stone : 디딤돌의 건널 수 있는 허용 수
+// k : 디딤돌의 최대 칸 수
+// result : 징검다리를 건널 수 있는 인원 수
+
  function solution(stones, k) {
+    // task 1. 
     var answer = 0;
     return answer;
 }
 
+const stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1];
+const k = 3
+
 const ans = solution(stones, k);
 console.log(ans);
+// to be : 3
