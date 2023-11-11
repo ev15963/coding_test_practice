a99606c97ff934854736398886ea2922ed51cb3d
nicky <ev15963@hs.ac.kr>
2023-11-11 08:28:33 +0000

    문제 정보 추가

--- programmers/150367_표현_가능한_이진트리/150367.js
+++ programmers/150367_표현_가능한_이진트리/150367.js
@@ -1,3 +1,10 @@
+/**
+ * 표현 가능한 이진트리
+ * URL : https://school.programmers.co.kr/learn/courses/30/lessons/150367
+ * @param {const} numbers - 입출력 예
+ * @returns {const} 
+ */
+
 function solution(numbers) {
     var answer = [];
     return answer;
