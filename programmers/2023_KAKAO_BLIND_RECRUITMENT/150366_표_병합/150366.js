07a6d60d32b43e6e05bcdb82e32c697ef97d9809
nicky <ev15963@hs.ac.kr>
2023-11-18 13:26:57 +0000

    150366 todo list

--- programmers/2023_KAKAO_BLIND_RECRUITMENT/150366_표_병합/150366.js
+++ programmers/2023_KAKAO_BLIND_RECRUITMENT/150366_표_병합/150366.js
@@ -4,7 +4,27 @@
  */
 
 function solution(commands) {
-    var answer = [];
+    // TODO 1. 50x50 크기의 표 초기화
+
+
+    // TODO 2. 특정 셀이 속한 그래프의 루트를 찾는 함수
+    function findRoot(r, c) {
+    }
+
+    // TODO 3. 두 셀을 병합하는 함수
+    function mergeCells(r1, c1, r2, c2) {
+    }
+
+    // TODO 4. 특정 셀의 병합을 해제하는 함수
+    function unmergeCell(r, c) {
+    }
+
+    
+    // TODO 5. 주어진 명령어를 실행하는 함수
+    function executeCommand(command) {
+    }
+
+    const answer = [];
     return answer;
 }
 
