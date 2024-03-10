b0d479dcb46469b05daec34469560790f9499e74
ev15963 <ev15963@hs.ac.kr>
2024-03-10 23:55:33 +0900

    217 solve

--- leecode/217_contains_duplicate.js
+++ leecode/217_contains_duplicate.js
@@ -3,6 +3,24 @@
 // 시간 복잡도는 O(n)이다.
 // 정렬은 퀵소트로 이루어진 Arrays.sort 메서드를 사용했다 [O(NlogN)]
 
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+
 // JAVA 예시
 // Set<Integer> duplicate = new HashSet<>();
 
@@ -13,3 +31,30 @@
 //     duplicate.add(num);
 // }
 // return false;
+
+// TODO 1 자바스크립트로 set 사용
+// URL : https://www.daleseo.com/js-set/
+// const set = new Set();
+// const numSet = new Set([1, 2, 3]);
+
+function solution() {
+
+    const duplicate = new Set();
+
+    const nums = [1, 2, 3, 1];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (duplicate.has(nums[i])) {
+            
+            return true;
+        }
+        duplicate.add(nums[i]);
+        duplicate;
+    };
+
+    return false;
+}
+
+const ans = solution();
+
+console.log(ans);
