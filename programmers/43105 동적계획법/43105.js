68abf4e473b7cdefb5611618ae836492682f7e4f
ev15963 <ev15963@hs.ac.kr>
2024-03-25 21:27:01 +0900

    경로 전체 호출해보기

--- programmers/43105 동적계획법/43105.js
+++ programmers/43105 동적계획법/43105.js
@@ -5,9 +5,10 @@ function solution(triangle) {
 
     // todo 1. 경우의 수의 합을 나열하기
     // point. 7은 단일 길이라 제외하고 시작
-    for (let i=1; i<triangle.length(); i++) {
-        for (let j=0; j<triangle[i].length(); j++) {
+    for (let i=1; i<triangle.length; i++) {
+        for (let j=0; j<triangle[i].length; j++) {
             // 7에서 3이나 8로 이동
+            console.log(triangle[i][j]);
             
         }
     }
