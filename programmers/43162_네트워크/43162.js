f852505fbe795d061aa589fb5a25e39c4a781202
ev15963 <ev15963@hs.ac.kr>
2024-03-26 08:32:39 +0900

    dfs 만들기

--- programmers/43162_네트워크/43162.js
+++ programmers/43162_네트워크/43162.js
@@ -1,3 +1,12 @@
+function DFS (n, computers, com, visited) {
+    visited[com] = true;
+    for (let i = 0; i < computers.length; i++) {
+        console.log('ddd');
+        console.log(computers[i], 'computers[i]');
+    }
+    // computer
+}
+
 function solution(n, computers) {
     var answer = 0;
     return answer;
