3576e39db54a5fc894bbda3c8391810b6e687b12
ev15963 <ev15963@hs.ac.kr>
2024-03-25 21:18:12 +0900

    todo 1. 경우의 수의 합 나열하기

--- programmers/43105 동적계획법/43105.js
+++ programmers/43105 동적계획법/43105.js
@@ -1,5 +1,17 @@
+const triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]];
+
 function solution(triangle) {
     var answer = 0;
-    
+
+    // todo 1. 경우의 수의 합을 나열하기
+    // point. 7은 단일 길이라 제외하고 시작
+    for (let i=1; i<triangle.length(); i++) {
+        for (let j=0; j<triangle[i].length(); j++) {
+            // 7에서 3이나 8로 이동
+            
+        }
+    }
+
+
     return answer;
 }
