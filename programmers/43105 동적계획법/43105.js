7854e1b3bdc9ad07a7b77f9f27d517828e5044a2
ev15963 <ev15963@hs.ac.kr>
2024-03-26 01:56:51 +0900

    배열에 담아서 최댓값 출력

--- programmers/43105 동적계획법/43105.js
+++ programmers/43105 동적계획법/43105.js
@@ -5,6 +5,7 @@ function solution(triangle) {
 
     // todo 1. 경우의 수의 합을 나열하기
     // point. 7은 단일 길이라 제외하고 시작
+    let triangle_array = [];
     for (let i=1; i<triangle.length; i++) {
         for (let j=0; j<triangle[i].length; j++) {
             // 7에서 3이나 8로 이동
@@ -16,20 +17,23 @@ function solution(triangle) {
             // 시작 영역
                 triangle[i][j] += triangle[i - 1][j];
                 console.log(triangle[i][j], 'if');
+                triangle_array.push(triangle[i][j]);
             } else if(j==triangle[i].length-1) {
             // 끝  
                 triangle[i][j] += triangle[i - 1][j - 1];
                 console.log(triangle[i][j], 'elif');
+                triangle_array.push(triangle[i][j]);
             } else {
             // 중간 영역
                 let max_num = Math.max(triangle[i-1][j-1], triangle[i-1][j]);
                 triangle[i][j] += max_num;
                 console.log(triangle[i][j], 'else');
+                triangle_array.push(triangle[i][j]);
             }
 
         }
     }
 
-
+    answer = Math.max(...triangle_array);
     return answer;
 }
