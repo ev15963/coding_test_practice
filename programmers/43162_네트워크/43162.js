dd3a7a23b4428e749282a624d2c84a0ab44fa4bc
ev15963 <ev15963@hs.ac.kr>
2024-03-26 08:47:50 +0900

    dfs로 연결

--- programmers/43162_네트워크/43162.js
+++ programmers/43162_네트워크/43162.js
@@ -1,7 +1,9 @@
+const n = 3;
+const computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]];
+
 function DFS (n, computers, com, visited) {
     visited[com] = true;
-    for (let i = 0; i < computers.length; i++) {
-        console.log('ddd');
+    for (let i=0; i<computers.length; i++) {
         console.log(computers[i], 'computers[i]');
     }
 }
@@ -9,10 +11,16 @@ function DFS (n, computers, com, visited) {
 function solution(n, computers) {
     var answer = 0;
     let visited = new Array();
-
-    for (let i=0; i< computer.length; i++) {
-        visited[i] = false;
+    
+    for (let i=0; i< computers.length; i++) {
+        visited[i] = false
     }
     
+    for(let i=0; i< computers.length; i++) {
+        if (visited[i] == false) {
+            answer ++;
+            DFS (n, computers, i, visited);
+        }
+    }
     return answer;
 }
