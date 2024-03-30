354b4625fa77d876a3c8156be9714e3fee95368d
ev15963 <ev15963@hs.ac.kr>
2024-03-30 23:56:39 +0900

    43238 ans init

--- programmers/43238_입국심사/43238.js
+++ programmers/43238_입국심사/43238.js
@@ -1,8 +1,12 @@
 const n = 6;
 const times = [7, 10];
-// to be 28
 
 function solution(n, times) {
     var answer = 0;
     return answer;
 }
+
+const ans = solution(n, times);
+
+console.log(ans, 'ans');
+// to be 28
