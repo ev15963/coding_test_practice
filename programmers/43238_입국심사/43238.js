4c1d4b2944d2cf330877f9589fd435e06e50ae64
ev15963 <ev15963@hs.ac.kr>
2024-03-31 17:18:07 +0900

    43238 이분 탐색

--- programmers/43238_입국심사/43238.js
+++ programmers/43238_입국심사/43238.js
@@ -3,6 +3,28 @@ const times = [7, 10];
 
 function solution(n, times) {
     var answer = 0;
+    
+    let left = 0;
+    let right = times.length -1;
+
+    while (left <= right) {
+        let mid = Math.floor(left + right) / 2;
+        
+        // TODO 1. 탐색값을 찾으면 멈춤
+        // TODO 2. 탐색값 < 중앙값 -> 끝점 갱신
+        // TODO 3. 중앙값 > 탐색값 -> 시작점 갱신
+        if (times[mid] == n) {
+            answer = n;
+            console.log(n, 'n');
+        } else if (n < times[mid]) {
+            right = mid - 1;
+        } else if (n > times[mid]) {
+            left = mid + 1;
+        }
+        
+        
+    }
+
     return answer;
 }
 
