5ad849e86083c7d7c4d96bcbfa40498bc8515da0
ev15963 <ev15963@hs.ac.kr>
2024-03-31 18:38:11 +0900

    무한루프 해결

--- programmers/43238_입국심사/43238.js
+++ programmers/43238_입국심사/43238.js
@@ -5,17 +5,18 @@ function solution(n, times) {
     var answer = 0;
     
     let left = 0;
-    let right = times.length -1;
-
+    let right = times.length - 1;
+    
     while (left <= right) {
-        let mid = Math.floor(left + right) / 2;
+        let mid = Math.floor((left + right) / 2);
         
         // TODO 1. 탐색값을 찾으면 멈춤
         // TODO 2. 탐색값 < 중앙값 -> 끝점 갱신
         // TODO 3. 중앙값 > 탐색값 -> 시작점 갱신
         if (times[mid] == n) {
-            answer = n;
+            answer += mid;
             console.log(n, 'n');
+            break;
         } else if (n < times[mid]) {
             right = mid - 1;
         } else if (n > times[mid]) {
