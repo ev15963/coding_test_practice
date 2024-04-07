4866205820302892fc7a719daa0272d5117b1a6c
ev15963 <ev15963@hs.ac.kr>
2024-04-07 12:55:24 +0900

    완료

--- programmers/43238_입국심사/43238.js
+++ programmers/43238_입국심사/43238.js
@@ -1,28 +1,35 @@
 const n = 6;
-const times = [7, 10];
-
+const times = [10, 7];
 function solution(n, times) {
-    var answer = 0;
-    
-    let left = 0;
-    let right = times.length - 1;
-    
+    // 함수를 넣어주면 숫자 순서대로 정렬
+    // https://velog.io/@ansrjsdn/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-level3-%EC%9E%85%EA%B5%AD%EC%8B%AC%EC%82%AC
+    times.sort((a, b) => a - b);
+    let left = 1;
+    let right = n * times[times.length -  1];
+    var answer = right;
+    console.log(right);
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-        
-        // TODO 1. 탐색값을 찾으면 멈춤
-        // TODO 2. 탐색값 < 중앙값 -> 끝점 갱신
-        // TODO 3. 중앙값 > 탐색값 -> 시작점 갱신
-        if (times[mid] == n) {
-            answer += mid;
-            console.log(n, 'n');
-            break;
-        } else if (n < times[mid]) {
+        console.log(mid, 'mid');
+        let people = 0;
+        times
+
+        for (let i = 0; i < times.length; i++) {
+            let value = times[i];
+            people += Math.floor(mid / value); // 한 사람당 몇명 할 수 있는지
+            people;
+            if (people >= n) {
+                console.log(people, n)
+                answer = Math.min(mid, answer); // 최솟값
+                break;
+            };
+        }
+
+        if (people >= n) {
             right = mid - 1;
-        } else if (n > times[mid]) {
+        } else {
             left = mid + 1;
         }
-        
         
     }
 
