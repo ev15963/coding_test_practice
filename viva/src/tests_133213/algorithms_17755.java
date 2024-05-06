a84ae5c8166ba5fedda1d5fac7a031fa9fbf1bcb
ev15963 <ev15963@hs.ac.kr>
2024-05-06 16:04:20 +0900

    17755 todo list

--- viva/src/tests_133213/algorithms_17755.java
+++ viva/src/tests_133213/algorithms_17755.java
@@ -9,6 +9,12 @@ public class algorithms_17755 {
         // taxFreeAmount : 비과세금액
         // serviceFee : 봉사료
         long answer = 0;
+
+        // TODO 1. 부가가체세는 과세금액의 10% 소숫점 첫재짜리에서 올림처리
+        // TODO 2. 과세금액은 공급가액에서 비과세 금액을 차감
+        // TODO 3. 공급가액 + 부가가치세 = 공급대가, 봉사료가 없는 공급대가는 주문금액과 같다.
+        // TODO 4. 공급대가 - 비과세금액 = 1원이면 부가가치세는 0원처리
+
         return answer;
     }
 }
