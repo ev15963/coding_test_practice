7f712abe0b966888a1babf86bf82b2c3748094c6
ev15963 <ev15963@hs.ac.kr>
2024-05-06 14:27:42 +0900

    algorithms_17755 init

---
+++ viva/src/tests_133213/algorithms_17755.java
@@ -0,0 +1,14 @@
+package tests_133213;
+
+import java.lang.Math;
+
+public class algorithms_17755 {
+
+    public long solution(long orderAmount, long taxFreeAmount, long serviceFee) {
+        // orderAmount : 주문금액
+        // taxFreeAmount : 비과세금액
+        // serviceFee : 봉사료
+        long answer = 0;
+        return answer;
+    }
+}
