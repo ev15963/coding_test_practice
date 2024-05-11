464fd0832ef994038113c1468836acf283e57eae
ev15963@hs.ac.kr <--unset>
2024-05-11 23:10:54 +0900

    algorithms_17766 init

--- viva/src/tests_133213/algorithms_17766.java
+++ viva/src/tests_133213/algorithms_17766.java
@@ -1,5 +1,20 @@
 package tests_133213;
 
 public class algorithms_17766 {
+    public static int solution(int money, int[][] stocks) {
+        // 가치의 총합
+        int totalValue = 0;
 
+        return totalValue;
+    }
+
+    public static void main(String[] args) {
+        int money1 = 10;
+        int[][] stocks1 = { { 1, 1 }, { 3, 5 }, { 3, 5 }, { 4, 9 } };
+        System.out.println("예시1 결과: " + solution(money1, stocks1)); // 출력: 6
+
+        int money2 = 30;
+        int[][] stocks2 = { { 1, 31 } };
+        System.out.println("예시2 결과: " + solution(money2, stocks2)); // 출력: 0
+    }
 }
