78ab437dfaacf63a7210a377ebd7cf6e78548672
ev15963 <ev15963@hs.ac.kr>
2024-05-19 22:24:03 +0900

    17766 done

--- viva/src/tests_133213/algorithms_17766.java
+++ viva/src/tests_133213/algorithms_17766.java
@@ -1,25 +1,38 @@
 package tests_133213;
 
+import java.util.Arrays;
+
 public class algorithms_17766 {
     public static int solution(int money, int[][] stocks) {
-        // 가치의 총합
-        int totalValue = 0;
+        // 가격 높은 순으로 배열
+        Arrays.sort(stocks, (a, b) -> b[1] - a[1]);
+        String text = Arrays.deepToString(stocks);
+        System.out.println(text);
+        System.out.println("*********************");
 
-        // TODO.1 가진 금액 중 구매할 수 있는 가치를 찾기
+        // 0 ~ 10이라는 배열 만들기
+        int[] money_arr = new int[money + 1];
+
         for (int[] stock : stocks) {
-            System.out.println(stock);
             int value = stock[0];
             int price = stock[1];
-
-            // 경우의수를 도입해서 적당한 price 선택하기
-            if (money >= price) {
-
+            System.out.println(Arrays.toString(stock));
+            // 배열을 뒤에서부터 순회하여 최대 가치를 업데이트
+            for (int j = money; j >= price; j--) {
+                System.out.println("---------------------------");
+                System.out.println("j");
+                System.out.println(j);
+                System.out.println("price");
+                System.out.println(price);
+                System.out.println("=============================");
+                System.out.println(money_arr[j]);
+                System.out.println(money_arr[j - price] + value);
+                money_arr[j] = Math.max(money_arr[j], money_arr[j - price] + value);
+                System.out.println(money_arr[j]);
             }
-            totalValue += value;
 
         }
-
-        return totalValue;
+        return money_arr[money];
     }
 
     public static void main(String[] args) {
