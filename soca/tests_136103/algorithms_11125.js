cdbfbc804f5153a2fc3a5c694c58432bd9c37ec4
ev15963@hs.ac.kr <--unset>
2024-05-30 06:52:00 +0900

    티켓정보 초기화

---
+++ soca/tests_136103/algorithms_11125.js
@@ -0,0 +1,23 @@
+function solution(tickets, roll, shop, money) {
+  // 티켓 정보 초기화 (티켓 정보 분리)
+  const ticketPrices = {};
+  for (const ticket of tickets) {
+    const [name, price] = ticket.split(" ");
+    ticketPrices[name] = Number(price);
+    console.log(ticketPrices[name], "ticketPrices[name]");
+    console.log(ticketPrices, "ticketPrices"); // { A: 1, B: 2, C: 5, D: 3 }
+  }
+
+
+  return answer;
+}
+// 테스트 케이스
+const tickets1 = ["A 1", "B 2", "C 5", "D 3"]; // 일반 티켓의 종류
+const roll1 = 10; // 상점 새로고침 비용
+const shop1 = [
+  ["B", "C", "B", "C"], // 2+5+2+5 = 14
+  ["A", "A", "A", "B"], // 1+1+1+2 = 5
+  ["D", "D", "C", "D"], // 3+3+5+3 = 14
+]; // 상점의 티켓 진열 예측
+const money1 = 30; //주어진 자금
+console.log(solution(tickets1, roll1, shop1, money1)); // 출력: 2 // 모을 수 있는 황금 티켓 개수의 최댓값
