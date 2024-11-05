93c13cefcf3679c557dd3dfbcb92c6d2d8911ca3
ev15963 <ev15963@hs.ac.kr>
2024-11-05 21:45:14 +0900

    P11660_구간합구하기2 test case 추가

--- do_it_algorithms_java/자료구조/P11660_구간합구하기2.java
+++ do_it_algorithms_java/자료구조/P11660_구간합구하기2.java
@@ -2,6 +2,15 @@ package 자료구조;
 
 public class P11660_구간합구하기2 {
     public static void main(String[] args) {
+        // test case
+        // 4 3
+        // 1 2 3 4
+        // 2 3 4 5
+        // 3 4 5 6
+        // 4 5 6 7
+        // 2 2 3 4 -> 27
+        // 3 4 3 4 -> 6
+        // 1 1 4 4 -> 64
 
     }
 }
