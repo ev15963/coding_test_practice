87dd0a56d13ea4b9c26a30399eb3bc501c1cd369
ev15963 <ev15963@hs.ac.kr>
2024-11-08 21:22:10 +0900

    P11660_구간합구하기 문법 오류 수정

--- do_it_algorithms_java/자료구조/P11660_구간합구하기2.java
+++ do_it_algorithms_java/자료구조/P11660_구간합구하기2.java
@@ -1,7 +1,11 @@
 package 자료구조;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P11660_구간합구하기2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // test case
         // 4 3
         // 1 2 3 4
@@ -13,18 +17,18 @@ public class P11660_구간합구하기2 {
         // 1 1 4 4 -> 64
 
         // 배열크기
-        // N(배열 크기) M(질의 수) 저장하기
+        // 크기) M(질의 수) 저장하기
+        // (N만큼 반복하기) {
         // for (N만큼 반복하기) {
-        //     for (N만큼 반복하기) {
-        //     원본 배열 저장하기
-        //     }
+        // 원본 배열 저장하기
+        // }
         // }
 
+        // (N만큼 반복하기) {
         // for (N만큼 반복하기) {
-        //     for (N만큼 반복하기) {
-        //     합 배열 저장하기
-        //     D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
-        //     }
+        // 합 배열 저장하기
+        // D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
+        // }
         // }
 
         // for (M만큼 반복하기) {
@@ -32,19 +36,17 @@ public class P11660_구간합구하기2 {
         // 결과 = D[x2][y2] - D[x1-1][y2] + D[x2][y1-1] + D[x1-1][y1-1];
         // }
 
-        BufferReader br = new BufferReader (new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         // 변수 선언
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 배열 입력과 동시에 합배열 구하기
-        int [][] S = new int[N+1][N+1];
-        for (int i = 0; i< N+1; i++) {
-            for (int j = 0; j < N+1; j++) {
-                
-            }
+        // 배열 입력과 동시에 합배열 구하 기
+        int[][] S = new int[N + 1][N + 1];
+
+        for (int j = 0; j < N + 1; j++) {
+
         }
     }
 }
