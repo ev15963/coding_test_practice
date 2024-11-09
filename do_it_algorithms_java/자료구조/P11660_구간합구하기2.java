c2fad9bd55d3f8ea6b58ef4d56db041912c8a921
ev15963 <ev15963@hs.ac.kr>
2024-11-09 21:53:17 +0900

    P11660_구간합구하기2 질의 계산 및 출력하기

--- do_it_algorithms_java/자료구조/P11660_구간합구하기2.java
+++ do_it_algorithms_java/자료구조/P11660_구간합구하기2.java
@@ -58,9 +58,20 @@ public class P11660_구간합구하기2 {
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                D[i][j] = D[i][j + 1] + D[i - 1][j] - D[i - 1][j - 1] + S[i][j];
-                System.out.println(D[i][j]);
+                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + S[i][j];
             }
+        }
+
+        // 질의 계산 및 출력하기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = D[x2][y2] - D[x1 - 1][y2] + D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            System.out.println(result);
         }
     }
 }
