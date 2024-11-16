d4f5de915ec88a012dc599e7942003f128c3deee
ev15963 <ev15963@hs.ac.kr>
2024-11-16 20:33:00 +0900

    P10986 나머지 합 변수 선언 합배열 선언

--- do_it_algorithms_java/자료구조/P10986_나머지합.java
+++ do_it_algorithms_java/자료구조/P10986_나머지합.java
@@ -1,6 +1,9 @@
 package 자료구조;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // TODO 1. 배열의 구간합 배열 생성하기
 // TODO 2. 구간합 배열의 나머지 배열 구하기
@@ -11,7 +14,16 @@ import java.util.Scanner;
 // 1 2 3 1 2
 
 public class P10986_나머지합 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 변수 선언
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 합배열 선언
+        int S[][] = new int[N][N];
 
     }
 }
