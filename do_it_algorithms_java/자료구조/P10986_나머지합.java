798a519b44f4f6c957143ae5400633e5f01334f2
ev15963 <ev15963@hs.ac.kr>
2024-11-16 20:06:11 +0900

    P10986 나머지 합 todo 작성

--- do_it_algorithms_java/자료구조/P10986_나머지합.java
+++ do_it_algorithms_java/자료구조/P10986_나머지합.java
@@ -1,5 +1,9 @@
 package 자료구조;
 
+// TODO 1. 배열의 구간합 배열 생성하기
+// TODO 2. 구간합 배열의 나머지 배열 구하기
+// TODO 3. 나머지 배열 요소가 0인 부분만 더하기
+
 public class P10986_나머지합 {
     public static void main(String[] args) {
 
