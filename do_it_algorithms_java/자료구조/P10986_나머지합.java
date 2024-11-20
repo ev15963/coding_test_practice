0391890b7754fa105e8d578aff6ae9ea1c9ef148
nicky <62686044+ev15963@users.noreply.github.com>
2024-11-20 15:59:34 +0900

    P10986_나머지합 result

--- do_it_algorithms_java/자료구조/P10986_나머지합.java
+++ do_it_algorithms_java/자료구조/P10986_나머지합.java
@@ -23,7 +23,34 @@ public class P10986_나머지합 {
         int M = Integer.parseInt(st.nextToken());
 
         // 합배열 선언
-        int S[][] = new int[N][N];
+        int S[] = new int[N];
 
+        // 나머지 배열 선언
+        int M[] = new int[N];
+
+        // M으로 나누어 떨어지는 구간의 개수
+        int result;
+
+        // 배열 돌려가며 구간합 구하기
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            // 구간합 배열 적재하기
+            if (i == 0)
+                S[i] = num;
+            else 
+                S[i] = S[i-1] + num;
+            
+            // M 배열도 같이 적재하기
+            M[i] = S[i] % M;
+        }
+
+        // M 배열의 배열 결과 중 0인 값의 S[i] 총 합계
+        for (int i = 0; i < N; i++) {
+            if (M[i] == 0) {
+                result += S[i];
+            }
+        }
+        return result;
     }
 }
