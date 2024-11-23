b7ee2208d2f6bf5cd90588aae89ba398e50ac340
ev15963 <ev15963@hs.ac.kr>
2024-11-23 15:12:25 +0900

    P10986 나머지 합 결과 출력되게 수정

--- do_it_algorithms_java/자료구조/P10986_나머지합.java
+++ do_it_algorithms_java/자료구조/P10986_나머지합.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
 // TODO 1. 배열의 구간합 배열 생성하기
 // TODO 2. 구간합 배열의 나머지 배열 구하기
 // TODO 3. 나머지 배열 요소가 0인 부분만 더하기
@@ -22,35 +21,32 @@ public class P10986_나머지합 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 합배열 선언
-        int S[] = new int[N];
+        // 합 배열 및 나머지 카운팅 배열
+        long[] S = new long[N + 1];
+        long[] count = new long[M]; // 나머지 개수를 저장할 배열
 
-        // 나머지 배열 선언
-        int M[] = new int[N];
+        // 입력값 읽기
+        st = new StringTokenizer(br.readLine());
 
-        // M으로 나누어 떨어지는 구간의 개수
-        int result;
-
-        // 배열 돌려가며 구간합 구하기
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             int num = Integer.parseInt(st.nextToken());
-
-            // 구간합 배열 적재하기
-            if (i == 0)
-                S[i] = num;
-            else 
-                S[i] = S[i-1] + num;
-            
-            // M 배열도 같이 적재하기
-            M[i] = S[i] % M;
+            S[i] = S[i - 1] + num; // 합 배열 생성
+            int remainder = (int) (S[i] % M); // 나머지 계산
+            if (remainder < 0)
+                remainder += M; // 음수 나머지 보정
+            count[remainder]++; // 나머지 개수 증가
         }
 
-        // M 배열의 배열 결과 중 0인 값의 S[i] 총 합계
-        for (int i = 0; i < N; i++) {
-            if (M[i] == 0) {
-                result += S[i];
+        // 나머지가 0인 구간의 개수 계산
+        long result = count[0]; // 나머지가 0인 경우는 바로 더함
+        for (int i = 0; i < M; i++) {
+            if (count[i] > 1) {
+                // 같은 나머지를 가진 부분합 중 2개를 선택하는 경우의 수 더하기
+                result += (count[i] * (count[i] - 1)) / 2;
             }
         }
-        return result;
+
+        // 결과 출력
+        System.out.println(result);
     }
 }
