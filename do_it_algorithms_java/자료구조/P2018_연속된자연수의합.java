7c189322e9f24c420a093e915727e14d31efe043
nicky <62686044+ev15963@users.noreply.github.com>
2024-11-28 13:20:27 +0900

    P2018_연속된자연수의합 슈도코드

--- do_it_algorithms_java/자료구조/P2018_연속된자연수의합.java
+++ do_it_algorithms_java/자료구조/P2018_연속된자연수의합.java
@@ -6,6 +6,16 @@ package 자료구조;
 // output
 // 4
 
+// 슈도 코드
+// N 변수 저장
+// 사용 변수 초기화 (count = 1, start_index = 1, end_index = 1, sum = 1)
+// while(end_index != N) {
+//    if (sum == N) count 증가, end_index 증가, sum값 변경
+//    else if (sum > N) sum값 변경, start_indes 증가
+//    else if (sum < N) end_index 증가, sum값 변경
+// }
+// count 출력하기
+
 public class P2018_연속된자연수의합 {
   public static void main(String[] args) {
     
