1cb2e8fae35989034fccccd084e2210663608ace
nicky <62686044+ev15963@users.noreply.github.com>
2024-12-10 08:53:08 +0900

    P1253_좋은수.java testcase

--- do_it_algorithms_java/자료구조/P1253_좋은수.java
+++ do_it_algorithms_java/자료구조/P1253_좋은수.java
@@ -1,7 +1,14 @@
 package 자료구조;
 
+// test case
+// input
+// 10 // 수의 개수
+// 1 2 3 4 5 6 7 8 9 10
+// output
+// 8
+
 public class P1253_좋은수 {
     public static void main(String[] args) {
-
+        
     }
 }
