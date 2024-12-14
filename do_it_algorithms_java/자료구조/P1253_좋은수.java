7c45008b6f72b6edbbbad9cdf886410e9984d430
ev15963 <ev15963@hs.ac.kr>
2024-12-14 17:01:14 +0900

    P1253 좋은수 문제 추가

--- do_it_algorithms_java/자료구조/P1253_좋은수.java
+++ do_it_algorithms_java/자료구조/P1253_좋은수.java
@@ -1,5 +1,8 @@
 package 자료구조;
 
+// 문제
+// 주어진 N개의 수에서 다른 두 수의 합으로 표현되는 수가 있다면 좋은 수
+
 // test case
 // input
 // 10 // 수의 개수
@@ -9,6 +12,6 @@ package 자료구조;
 
 public class P1253_좋은수 {
     public static void main(String[] args) {
-        
+
     }
 }
