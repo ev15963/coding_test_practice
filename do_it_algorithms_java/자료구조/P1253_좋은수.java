c85bd0665f405f130ea4c652ce882f4bc5da041f
ev15963 <ev15963@hs.ac.kr>
2024-12-14 17:51:17 +0900

    input 값 변수할당

--- do_it_algorithms_java/자료구조/P1253_좋은수.java
+++ do_it_algorithms_java/자료구조/P1253_좋은수.java
@@ -1,5 +1,9 @@
 package 자료구조;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // 문제
 // 주어진 N개의 수에서 다른 두 수의 합으로 표현되는 수가 있다면 좋은 수
 
@@ -11,7 +15,18 @@ package 자료구조;
 // 8
 
 public class P1253_좋은수 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+
+        int S[] = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            S[i] = Integer.parseInt(st.nextToken());
+
+        }
 
     }
 }
