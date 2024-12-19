aa78e75c8f9b09e718eefb49c42cb031f27ed8c4
nicky <62686044+ev15963@users.noreply.github.com>
2024-12-19 15:47:38 +0900

    P12891_DNA_비밀번호.java init

---
+++ do_it_algorithms_java/자료구조/P12891_DNA_비밀번호.java
@@ -0,0 +1,14 @@
+package 자료구조;
+
+// test case
+// input
+// 9 8
+// CCTGGATTG
+// 2 0 1 1
+// output
+// 0
+
+public class P12891_DNA_비밀번호 {
+    public static void main(String[] args) throws IOException {
+    }
+}
