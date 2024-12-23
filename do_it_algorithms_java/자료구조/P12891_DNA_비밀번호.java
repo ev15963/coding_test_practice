c69673597290e4ad54b45867fc6fa0023fbbbfa5
nicky <62686044+ev15963@users.noreply.github.com>
2024-12-23 15:40:55 +0900

    P12891_DNA_비밀번호 최소 필요 갯수 배열 선언 및 초기화

--- do_it_algorithms_java/자료구조/P12891_DNA_비밀번호.java
+++ do_it_algorithms_java/자료구조/P12891_DNA_비밀번호.java
@@ -23,6 +23,13 @@ public class P12891_DNA_비밀번호 {
 
         // 각 문자 최소 갯수 배열
         st = new StringTokenizer(br.readLine());
-        
+
+        // 슬라이딩 윈도우 4 길이 배열
+        int[] checkArr = new int[4];
+
+        // 2 0 1 1 담기
+        for (int i=0; i<4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+        }
     }
 }
