5b18633d9abb8cb7e4cd88eade0e3f051e30f69f
nicky <62686044+ev15963@users.noreply.github.com>
2024-12-24 13:12:16 +0900

    P11003_최솟값_찾기 init

---
+++ do_it_algorithms_java/자료구조/P11003_최솟값_찾기.java
@@ -0,0 +1,15 @@
+package 자료구조;
+
+// test case
+// input
+// 12 3
+// 1 5 2 3 6 2 3 7 3 5 2 6
+// output
+// 1 1 1 2 2 2 2 2 3 3 2 2
+
+
+public class P11003_최솟값_찾기 {
+    public static void main(String[] args) throws IOException {
+
+  }
+}
