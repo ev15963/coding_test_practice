bb8c415f95b64983dceed869b510f3188a061b06
nicky <62686044+ev15963@users.noreply.github.com>
2024-12-26 17:22:34 +0900

    P11003_최솟값_찾기 N, L 선언

--- do_it_algorithms_java/자료구조/P11003_최솟값_찾기.java
+++ do_it_algorithms_java/자료구조/P11003_최솟값_찾기.java
@@ -12,5 +12,11 @@ public class P11003_최솟값_찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N : 배열의 길이, 여기선 12
+        int N = Integer.parseInt(st.nextToken());
+
+        // L : 배열의 수를 비교할 최대 길이, 여기선 3
+        int L = Integer.parseInt(st.nextToken());
   }
 }
