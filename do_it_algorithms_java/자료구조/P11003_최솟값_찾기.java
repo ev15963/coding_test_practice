8d963e1b4ab63257e8e86f6c084ba7c140fbaaee
ev15963 <ev15963@hs.ac.kr>
2025-01-04 16:43:03 +0900

    P11003_최솟값 찾기 버퍼에 넣고 한번에 출력, deque를 이용한 방식 설계

--- do_it_algorithms_java/자료구조/P11003_최솟값_찾기.java
+++ do_it_algorithms_java/자료구조/P11003_최솟값_찾기.java
@@ -1,8 +1,10 @@
 package 자료구조;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
@@ -24,6 +26,8 @@ public class P11003_최솟값_찾기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 버퍼에 넣고 한번에 출력하기 (fast io)
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         // N : 배열의 길이, 여기선 12
         int N = Integer.parseInt(st.nextToken());
 
@@ -37,7 +41,10 @@ public class P11003_최솟값_찾기 {
 
         for (int i = 0; i < N; i++) {
             int nowNum = Integer.parseInt(st.nextToken());
-
+            // 덱의 마지막 위치 부터 now 보다 큰 값은 덱에서 제거
+            // 덱의 마지막 위치에 now값 저장하기
+            // 덱의 1번째 위치에서부터 L의 범위를 벗어난 값(index < now index - L)을 덱에서 제거
+            // 덱의 1번째 데이터 출력하기
         }
     }
 }
