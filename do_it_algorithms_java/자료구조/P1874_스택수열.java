ec96f15f9f228f1f5820f504f4a165f7c4d5c16e
nicky <62686044+ev15963@users.noreply.github.com>
2025-01-11 16:34:19 +0900

    P1874_스택수열 test case 추가

--- do_it_algorithms_java/자료구조/P1874_스택수열.java
+++ do_it_algorithms_java/자료구조/P1874_스택수열.java
@@ -1,5 +1,35 @@
 package 자료구조;
 
+// test case
+// input
+// 8 // 수열의 갯수
+// 4 // 수열
+// 3 
+// 6
+// 8
+// 7
+// 5
+// 2
+// 1
+// output
+// +
+// +
+// +
+// +
+// -
+// -
+// +
+// +
+// -
+// =
+// +
+// -
+// -
+// -
+// -
+// -
+
+
 public class P1874_스택수열 {
     public static void main(String[] args) {
     }
