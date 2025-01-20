580b298ddfa6c2043278eb27a63550b76706a020
ev15963 <ev15963@hs.ac.kr>
2025-01-20 15:09:25 +0900

    P17298_오큰수 입력값 받기

--- do_it_algorithms_java/자료구조/P17298_오큰수.java
+++ do_it_algorithms_java/자료구조/P17298_오큰수.java
@@ -1,5 +1,11 @@
 package 자료구조;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 // input
 // 4 // 수열의 크기
 // 3 5 2 7
@@ -8,6 +14,17 @@ package 자료구조;
 // 5 7 7 -1
 
 public class P17298_오큰수 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 입력값 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer sb = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            // 수열의 크기만큼 입력값 받기
+            arr[i] = Integer.parseInt(sb.nextToken());
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
