cf9fc315cf618c51c5cee635568e776c1e9ab1ea
ev15963 <ev15963@hs.ac.kr>
2025-01-20 20:24:42 +0900

    P17298_오큰수 오큰수 여부에 따라 수열 추가

--- do_it_algorithms_java/자료구조/P17298_오큰수.java
+++ do_it_algorithms_java/자료구조/P17298_오큰수.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 // input
@@ -25,6 +26,27 @@ public class P17298_오큰수 {
             // 수열의 크기만큼 입력값 받기
             arr[i] = Integer.parseInt(sb.nextToken());
         }
-        System.out.println(Arrays.toString(arr));
+        // 수열의 인덱스 출력
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        // 오큰수일 경우 자기 위치에 삽입 후 스택에 추가, 아닐 경우 그냥 스택에 추가
+        for (int i = 1; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            // 오큰수 이면 인덱스 스택이 비었으므로 추가
+            // 오큰수가 아니면 스택에 추가
+            stack.push(i);
+        }
+        System.out.println(Arrays.toString(arr)); // [5, 7, 7, 7]
+        System.out.println(Arrays.toString(stack.toArray())); // [3]
+
+        // 오큰수가 없는 경우 (스택 인덱스 기준) -1 출력
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1; // stack = 3 -> arr[3] = -1
+        }
+        System.out.println(Arrays.toString(arr)); // [5, 7, 7, -1]
+
     }
 }
