a3627ddbde19f41c7b9589a826705605d1b7ac7f
ev15963 <ev15963@hs.ac.kr>
2025-01-20 21:08:36 +0900

    P2164_카드 카드의 개수 입력시 배열출력

--- do_it_algorithms_java/자료구조/P2164_카드.java
+++ do_it_algorithms_java/자료구조/P2164_카드.java
@@ -1,5 +1,11 @@
 package 자료구조;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 // test case
 // input
 // 6 // 카드의 개수
@@ -7,7 +13,18 @@ package 자료구조;
 // output
 // 4
 public class P2164_카드 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // 배열 만들기
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            // 배열에 값 넣기
+            queue.add(i + 1);
+        }
+        System.out.println(queue.toString());
 
     }
 }
