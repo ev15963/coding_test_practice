151d1bfbcb42ec3da93784c86daab415825d5068
ev15963 <ev15963@hs.ac.kr>
2025-01-21 15:17:42 +0900

    P11286_절댓값힙 입력 받은거 배열 만들기

--- do_it_algorithms_java/자료구조/P11286_절댓값힙.java
+++ do_it_algorithms_java/자료구조/P11286_절댓값힙.java
@@ -1,5 +1,11 @@
 package 자료구조;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 // test case
 // input
 // 18
@@ -35,7 +41,15 @@ package 자료구조;
 // 0
 
 public class P11286_절댓값힙 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
