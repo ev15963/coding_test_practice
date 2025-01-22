a0aa217985c7a7fb125eb00d201eec34e025420b
ev15963 <ev15963@hs.ac.kr>
2025-01-22 18:13:02 +0900

    P1377_버블소트1 test case

--- do_it_algorithms_java/정렬/P1377_버블소트1.java
+++ do_it_algorithms_java/정렬/P1377_버블소트1.java
@@ -1,5 +1,17 @@
 package 정렬;
 
+// test case
+// input
+// 5 // 배열의 크기
+// 10
+// 1
+// 5
+// 2
+// 3
+
+// output
+// 3
+
 public class P1377_버블소트1 {
     public static void main(String[] args) {
 
