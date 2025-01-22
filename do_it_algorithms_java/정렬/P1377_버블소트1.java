8808c717d1fb1cdf914ae758903e631554125ea2
ev15963 <ev15963@hs.ac.kr>
2025-01-22 18:31:17 +0900

    P1377_버블소트1 입력받은 값 출력

--- do_it_algorithms_java/정렬/P1377_버블소트1.java
+++ do_it_algorithms_java/정렬/P1377_버블소트1.java
@@ -1,5 +1,10 @@
 package 정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 // test case
 // input
 // 5 // 배열의 크기
@@ -13,7 +18,16 @@ package 정렬;
 // 3
 
 public class P1377_버블소트1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
