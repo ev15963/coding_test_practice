ff5fc6d58a80d17b5a16b7eadd880ed8d52d7c39
ev15963 <ev15963@hs.ac.kr>
2025-01-22 17:58:52 +0900

    P2750_수정렬하기 버블 정렬 뒤 호출

--- do_it_algorithms_java/정렬/P2750_수정렬하기.java
+++ do_it_algorithms_java/정렬/P2750_수정렬하기.java
@@ -34,6 +34,17 @@ public class P2750_수정렬하기 {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+        // 버블 정렬
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
         System.out.println(Arrays.toString(arr));
     }
 }
