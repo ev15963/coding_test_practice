4a9b3fa0648f35d1d16878d2d42ac0fb697dc381
ev15963 <ev15963@hs.ac.kr>
2025-01-22 17:26:46 +0900

    P2750_수정렬하기 입력된 값을 배열로 출력

--- do_it_algorithms_java/정렬/P2750_수정렬하기.java
+++ do_it_algorithms_java/정렬/P2750_수정렬하기.java
@@ -1,5 +1,10 @@
 package 정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 // test case
 // input
 // 5 // 수의 개수
@@ -17,7 +22,18 @@ package 정렬;
 // 5
 
 public class P2750_수정렬하기 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 수의 개수
+        int N = Integer.parseInt(br.readLine());
 
+        // 배열
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
