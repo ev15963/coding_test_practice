776eb1e9e17c43046a6c4b8fb76fef4fcb971450
ev15963 <ev15963@hs.ac.kr>
2025-01-22 17:15:04 +0900

    P2750_수정렬하기 test case

--- do_it_algorithms_java/정렬/P2750_수정렬하기.java
+++ do_it_algorithms_java/정렬/P2750_수정렬하기.java
@@ -1,5 +1,21 @@
 package 정렬;
 
+// test case
+// input
+// 5 // 수의 개수
+// 5
+// 2
+// 3
+// 4
+// 1
+
+// output
+// 1
+// 2
+// 3
+// 4
+// 5
+
 public class P2750_수정렬하기 {
     public static void main(String[] args) {
 
