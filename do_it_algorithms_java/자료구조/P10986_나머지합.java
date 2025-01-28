b5b26970e522c844ee230138d4430ad187b7f5ef
ev15963 <ev15963@hs.ac.kr>
2025-01-28 18:06:37 +0900

    P1377_버블소트1 불필요한 max 값 제거

--- do_it_algorithms_java/자료구조/P10986_나머지합.java
+++ do_it_algorithms_java/자료구조/P10986_나머지합.java
@@ -21,9 +21,9 @@ public class P10986_나머지합 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 합 배열 및 나머지 카운팅 배열
+        // 합 배열 및 나머지 카운팅 결과 값
         long[] S = new long[N + 1];
-        long[] count = new long[M]; // 나머지 개수를 저장할 배열
+        long result = 0;
 
         // 입력값 읽기
         st = new StringTokenizer(br.readLine());
@@ -34,16 +34,8 @@ public class P10986_나머지합 {
             int remainder = (int) (S[i] % M); // 나머지 계산
             if (remainder < 0)
                 remainder += M; // 음수 나머지 보정
-            count[remainder]++; // 나머지 개수 증가
-        }
-
-        // 나머지가 0인 구간의 개수 계산
-        long result = count[0]; // 나머지가 0인 경우는 바로 더함
-        for (int i = 0; i < M; i++) {
-            if (count[i] > 1) {
-                // 같은 나머지를 가진 부분합 중 2개를 선택하는 경우의 수 더하기
-                result += (count[i] * (count[i] - 1)) / 2;
-            }
+            if (remainder == 0)
+                result++; // 나머지 개수 증가
         }
 
         // 결과 출력
