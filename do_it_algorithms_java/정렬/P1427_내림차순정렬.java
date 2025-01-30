260705b9a50378de026a20fe0d7b4790d9409ccf
ev15963 <ev15963@hs.ac.kr>
2025-01-30 10:43:56 +0900

    P1427_내림차순정렬 arr로 출력

--- do_it_algorithms_java/정렬/P1427_내림차순정렬.java
+++ do_it_algorithms_java/정렬/P1427_내림차순정렬.java
@@ -1,7 +1,27 @@
 package 정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// test case
+// input
+// 2143
+
+// output
+// 4321
+
 public class P1427_내림차순정렬 {
-    public static void main(String[] args) {
-        
-    } 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int[] A = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+
+        System.out.println(Arrays.toString(A));
+    }
 }
