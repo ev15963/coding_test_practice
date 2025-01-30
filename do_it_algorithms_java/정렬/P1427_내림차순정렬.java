c0718c04a21e8a70dd6a6c74b951cd764ed4d613
ev15963 <ev15963@hs.ac.kr>
2025-01-30 10:44:24 +0900

    P1427_내림차순정렬 A -> arr 변경 누락

--- do_it_algorithms_java/정렬/P1427_내림차순정렬.java
+++ do_it_algorithms_java/정렬/P1427_내림차순정렬.java
@@ -17,11 +17,11 @@ public class P1427_내림차순정렬 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        int[] A = new int[str.length()];
+        int[] arr = new int[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            A[i] = Integer.parseInt(str.substring(i, i + 1));
+            arr[i] = Integer.parseInt(str.substring(i, i + 1));
         }
 
-        System.out.println(Arrays.toString(A));
+        System.out.println(Arrays.toString(arr));
     }
 }
