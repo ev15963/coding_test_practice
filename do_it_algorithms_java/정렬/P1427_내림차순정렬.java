f573ea89b70ca24437d3a9eba640cbab0607a77a
ev15963 <ev15963@hs.ac.kr>
2025-01-31 15:56:26 +0900

    P1427_내림차순정렬 내림차순 정렬

--- do_it_algorithms_java/정렬/P1427_내림차순정렬.java
+++ do_it_algorithms_java/정렬/P1427_내림차순정렬.java
@@ -22,6 +22,23 @@ public class P1427_내림차순정렬 {
             arr[i] = Integer.parseInt(str.substring(i, i + 1));
         }
 
-        System.out.println(Arrays.toString(arr));
+        // 내림차순 선택 정렬 구현
+        for (int i = 0; i < str.length(); i++) {
+            int max = i;
+            for (int j = i + 1; j < str.length(); j++) {
+                // max 값과 비교해서 바꿔야한다.
+                if (arr[max] < arr[j]) {
+                    max = j;
+                }
+            }
+            if (arr[i] < arr[max]) {
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(arr[i]);
+        }
     }
 }
