6c56ae6e52c26dd45c6a00fd887508575dd00806
ev15963 <ev15963@hs.ac.kr>
2025-02-01 17:43:41 +0900

    P11004_K번째수 test case

--- do_it_algorithms_java/정렬/P11004_K번째수.java
+++ do_it_algorithms_java/정렬/P11004_K번째수.java
@@ -1,7 +1,15 @@
 package 정렬;
 
+// test case
+// input
+// 5 2 // 데이터 개수, K번째 수
+// 4 1 2 3 5
+
+// output
+// 2
+
 public class P11004_K번째수 {
     public static void main(String[] args) {
-        
+
     }
 }
