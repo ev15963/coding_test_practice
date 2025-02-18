d796316c195aadc11354f4c883a8b42b9af96dd6
ev15963 <ev15963@hs.ac.kr>
2025-02-18 18:26:00 +0900

    P2751_수정렬하기2 병합정렬 : 중간 인덱스 구하기, 재귀 반복, tmp 배열 복사

--- do_it_algorithms_java/정렬/P2751_수정렬하기2.java
+++ do_it_algorithms_java/정렬/P2751_수정렬하기2.java
@@ -22,14 +22,36 @@ import java.util.Arrays;
 // 5
 
 public class P2751_수정렬하기2 {
+    // mergeSort 함수 생성으로 인한 변수 상위 선언
+    static int[] arr;
+    static int[] tmp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        arr = new int[N];
+        tmp = new int[N];
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
         System.out.println(Arrays.toString(arr));
+
+    }
+
+    // 병합 정렬
+    public static void mergeSort(int start, int end) {
+        // 중간 인덱스 구하기
+        int middle = (start + end) / 2;
+
+        // 재귀로 반복 0 ~middle, middle+1 ~ end
+        mergeSort(start, middle);
+        mergeSort(middle + 1, end);
+
+        // tmp 배열 복사
+        for (int i = start; i <= end; i++) {
+            tmp[i] = arr[i];
+        }
     }
 }
