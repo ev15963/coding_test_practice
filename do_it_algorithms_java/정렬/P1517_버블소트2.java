4ed693cfcc76c08bfedcc67d9e1eda9e1cb33976
ev15963 <ev15963@hs.ac.kr>
2025-02-24 17:20:21 +0900

    P1517_버블소트2 입력된거 출력

--- do_it_algorithms_java/정렬/P1517_버블소트2.java
+++ do_it_algorithms_java/정렬/P1517_버블소트2.java
@@ -1,5 +1,11 @@
 package 정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 // test case
 // input
 // 8 // 수의 개수
@@ -8,7 +14,15 @@ package 정렬;
 // 11
 
 public class P1517_버블소트2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
+        System.out.println(Arrays.toString(arr));
     }
 }
