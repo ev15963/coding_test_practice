720851459134535ec5aa9f6418b34bf3a9ada70e
ev15963 <ev15963@hs.ac.kr>
2025-02-24 18:17:38 +0900

    P1517_버블소트2 머지소트로 정렬하기

--- do_it_algorithms_java/정렬/P1517_버블소트2.java
+++ do_it_algorithms_java/정렬/P1517_버블소트2.java
@@ -20,11 +20,15 @@ public class P1517_버블소트2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int arr[] = new int[N];
+        arr = new int[N];
+        tmp = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        mergeSort(0, N - 1);
 
         System.out.println(Arrays.toString(arr));
     }
