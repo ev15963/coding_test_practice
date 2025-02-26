4ea7b2cf11766612a73d689fcab4a8fb3ac1cf6c
nicky <62686044+ev15963@users.noreply.github.com>
2025-02-26 18:08:31 +0900

    P10989_수정렬하기3 입력한 값 배열로 받기 & 기수정렬 클래스 생성

--- do_it_algorithms_java/정렬/P10989_수정렬하기3.java
+++ do_it_algorithms_java/정렬/P10989_수정렬하기3.java
@@ -3,7 +3,7 @@ package 정렬;
 // test case
 
 // input
-// 11
+// 11 // 배열의 길이
 // 215
 // 15
 // 344
@@ -33,5 +33,13 @@ public class P10989_수정렬하기3 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        // 기수정렬
+        RadixSort(A, 5);
     }
 }
