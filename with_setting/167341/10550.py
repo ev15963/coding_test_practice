2b9153f6d37d71fbef5c594f21c5d05f58c33943
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-03 15:53:33 +0900

    10550.py test case init

---
+++ with_setting/167341/10550.py
@@ -0,0 +1,9 @@
+def solution(pouches):
+    return result
+
+pouches = ["cab", "adaaa", "e"] #3 
+# pouches = ["aabb", "baba"] #0 
+# pouches = ["d", "a", "e", "d", "abdcc"] #3
+# pouches = ["a"] #1
+ans = solution(pouches)
+print(ans)
