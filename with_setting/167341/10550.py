14959b5aabedcc374cfb0d5d01c5cf37eb7521b7
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-04 15:31:14 +0900

    10550.py 낱말을 abcde 문자열로 순화하면서 진행

--- with_setting/167341/10550.py
+++ with_setting/167341/10550.py
@@ -1,5 +1,10 @@
+from collections import Counter
+
 def solution(pouches):
-    return result
+
+    max_count = 0;
+    # todo 1. 낱말을 abcde 문자열로 순회하며 진행
+    for flavor in 'abcde':
 
 pouches = ["cab", "adaaa", "e"] #3 
 # pouches = ["aabb", "baba"] #0 
