b8c0ccf20e304b7dbb8fb7491002c97a17b39a03
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-04 15:37:47 +0900

    10550.py 기여도 구하기

--- with_setting/167341/10550.py
+++ with_setting/167341/10550.py
@@ -5,6 +5,19 @@ def solution(pouches):
     max_count = 0;
     # todo 1. 낱말을 abcde 문자열로 순회하며 진행
     for flavor in 'abcde':
+        # todo 2. contributions 기여도 구하기
+        contributions = [];
+        for pouch in pouches:
+            counter = Counter(pouch)
+            this = counter[flavor];
+            etc = sum(counter.values()) - this;
+            contribution = this - etc;
+            contributions.append(contribution);
+            contributions.sort(reverse=True);
+            
+        max_count = max(max_count, count);
+            
+    return max_count;
 
 pouches = ["cab", "adaaa", "e"] #3 
 # pouches = ["aabb", "baba"] #0 
