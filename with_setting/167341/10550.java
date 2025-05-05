22d2b627589bcbee24bee7ffaf895b6d3ed20a76
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-05 15:49:19 +0900

    10550.java test case init

--- with_setting/167341/10550.java
+++ with_setting/167341/10550.java
@@ -4,7 +4,15 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static int solution(String[] pouches) {
+        int answer = 0;
+        return answer;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] pouches = {"cab", "adaaa", "e"}; // to be 3;
+        // String[] pouches = {"aabb", "baba"}; // to be 0;
+        // String[] pouches = {"d", "a", "e", "d", "abdcc"}; // to be 3;
+        // String[] pouches = {"a"}; // to be 1;
+        System.out.println(solution(pouches));
     }
 }
