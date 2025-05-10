9efb3bdf25b7d48bcd67048f43cabd2b4c4463ea
ev15963 <ev15963@hs.ac.kr>
2025-05-10 22:56:45 +0900

    1697 testcase init

--- backjoon/backjoon_1697.java
+++ backjoon/backjoon_1697.java
@@ -4,7 +4,15 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static int solution(int N, int K) {
+
+        return N;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int N = 5;
+        int K = 17;
+        int sol = solution(N, K);
+        System.out.println(sol); // to be 4
     }
 }
