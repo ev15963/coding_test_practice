3b119fbf709313a88f4ec7b63d8bfffd49efeafb
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-12 18:23:28 +0900

    10616.java solution class, test case init

--- with_setting/167341/10616.java
+++ with_setting/167341/10616.java
@@ -4,7 +4,12 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static boolean solution(int capacity, int[][] routes) {
+        boolean answer = true;
+        return answer;
+    }
     public static void main(String[] args) {
-      
+        int[][] routes = {{3, 2, 6}, {5, 1, 4}, {1, 7, 13}};
+        System.out.println(solution(9, routes)); // to be true
     }
 }
