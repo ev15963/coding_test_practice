1963229cd074f5111d442890e57120c4fb07e96c
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-12 20:03:50 +0900

    10616.java distance 배열 기반으로 누적합을 구하며 최대용량이랑 비교

--- with_setting/167341/10616.java
+++ with_setting/167341/10616.java
@@ -7,6 +7,9 @@ class Main {
     public static boolean solution(int capacity, int[][] routes) {
         // 0~1000 거리 선언
         int[] distance = new int[1001];
+
+        // 최대 경로 구하기
+        int maxDistance = 0;
         
         for (int[] route : routes) {
             int get = route[0];
@@ -16,12 +19,24 @@ class Main {
             // 선물 들고 가는 경로 남기기
             distance[start] += get;
             distance[end] -= get;
+
+            maxDistance = Math.max(maxDistance, end);
         }
 
-        System.out.println(Arrays.toString(distance));
+        int currentGift = 0;
+        // 최대 용량이 넘을 경우 false
+        for (int i = 0; i < maxDistance+1; i++) {
+            // 누적해서 더하기
+            currentGift += distance[i];
+            
+            if (capacity < currentGift) {
+                return false;
+            }
+        }
         
         return true;
     }
+    
     public static void main(String[] args) {
         int[][] routes = {{3, 2, 6}, {5, 1, 4}, {1, 7, 13}};
         System.out.println(solution(9, routes)); // to be true
