763b9605542ddb244b5ae5b425afa8fce72ba488
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-12 20:58:15 +0900

    11125.java test case init

--- with_setting/136103/11125.java
+++ with_setting/136103/11125.java
@@ -4,7 +4,16 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public int solution(String[] tickets, int roll, String[][] shop, int money) {
+        
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // to be 2;
+        String[] tickets = {"A 1", "B 2", "C 5", "D 3"};
+        int roll = 10;
+        String[][] shop = {{"B", "C", "B", "C"}, {"A", "A", "A", "B"}, {"D", "D", "C", "D"}};
+        int money = 30;
+        
+        System.out.println(solution(tickets, roll, shop, money));
     }
 }
