957dc561fa41840b0302bea283703ca28aa61342
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-12 19:33:31 +0900

    10616.java 선물 증량 정보를 distance 배열로 남기기

--- with_setting/167341/10616.java
+++ with_setting/167341/10616.java
@@ -5,8 +5,22 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     public static boolean solution(int capacity, int[][] routes) {
-        boolean answer = true;
-        return answer;
+        // 0~1000 거리 선언
+        int[] distance = new int[1001];
+        
+        for (int[] route : routes) {
+            int get = route[0];
+            int start = route[1];
+            int end = route[2];
+
+            // 선물 들고 가는 경로 남기기
+            distance[start] += get;
+            distance[end] -= get;
+        }
+
+        System.out.println(Arrays.toString(distance));
+        
+        return true;
     }
     public static void main(String[] args) {
         int[][] routes = {{3, 2, 6}, {5, 1, 4}, {1, 7, 13}};
