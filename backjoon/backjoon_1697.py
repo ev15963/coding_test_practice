8e5351822ba179d84be33e9b360c67cd84dbb1d0
nicky <62686044+ev15963@users.noreply.github.com>
2025-05-17 16:51:04 +0900

    backjoon_1697.py 이동 or 순간이동 하면서 방문 하지 않은 곳만 찾아가기 진행중

--- backjoon/backjoon_1697.py
+++ backjoon/backjoon_1697.py
@@ -21,13 +21,20 @@ def solution(N, K):
     while (queue):
         position = queue.popleft();
         depth = ++seconds;
-
+        
         # todo 5. 만약 도착지점과 같다면 시간(깊이) 반환
         if (position == K):
             return depth;
+
+        # todo 6. 이동 or 순간이동 하면서 방문 하지 않은 곳만 찾아가기
+        nextSteps = [ position-1, position+1, position*2 ];
+
+        for nextStep in nextSteps:
+            if nextStep >= 0 and nextStep < len(visited) and not visited[nextStep]:
+                visited[nextStep] = True;
+                queue.insert(nextStep);
     
-    
-    return 0;
+    return depth;
 
 N = 5;
 K = 17;
