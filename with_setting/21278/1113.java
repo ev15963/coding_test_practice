d6bfa46aeee83319259d2b47ab20bf14c014d6de
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-02 20:09:20 +0900

    1113.java test case init

--- with_setting/21278/1113.java
+++ with_setting/21278/1113.java
@@ -4,7 +4,21 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static int solution(int[][] office, int k) {
+        int answer = -1;
+        return answer;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 예제 1
+        int[][] office1 = {{1,0,0,0},{0,0,0,1},{0,0,1,0},{0,1,1,0}};
+        int k1 = 2;
+        System.out.println(solution(office1, k1));  // to be 3
+        
+        // // 예제 2
+        // int[][] office2 = [[1,0,0],[0,0,1],[1,0,0]];
+        // int k2 = 2;
+        // System.out.println(solution(office2, k2));  // to be 1
+
     }
 }
