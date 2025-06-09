021c537d9d479860073db087a3c535ded74a9492
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-09 17:02:25 +0900

    1113.java office 범위 내 알맞게 정보 찾기

--- with_setting/21278/1113.java
+++ with_setting/21278/1113.java
@@ -5,25 +5,36 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     public static int solution(int[][] office, int k) {
-        int answer = -1;
-        return answer;
-    }
-    
-    public static int solution(int[][] office, int k) {
+
         int len = office.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
+        int maxCount = 0;
+        for (int i = 0; i < len - k + 1; i++) {
+            for (int j = 0; j < len - k + 1; j++) {
                 int count = 0;
-                for (int x = 0; x < len - k + 1; x++) {
-                    for (int y = 0; y < len - k + 1; y++) {
+                for (int x = i; x < i + k; x++) {
+                    for (int y = j; y < j + k; y++) {
                         if (office[x][y] == 1) {
                             count += 1;
                         }
                     }  
                 }
+                maxCount = Math.max(count, maxCount);
             }
         }
         
-        return -1;
+        return maxCount;
+    }
+    
+    public static void main(String[] args) {
+        // 예제 1
+        // int[][] office1 = {{1,0,0,0},{0,0,0,1},{0,0,1,0},{0,1,1,0}};
+        // int k1 = 2;
+        // System.out.println(solution(office1, k1));  // to be 3
+        
+        // // 예제 2
+        int[][] office2 = {{1,0,0},{0,0,1},{1,0,0}};
+        int k2 = 2;
+        System.out.println(solution(office2, k2));  // to be 1
+
     }
 }
