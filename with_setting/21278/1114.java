c5910471d99debc9c3182d75e533fc4d11c351f7
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-10 19:37:57 +0900

    1114.java solution add, testcase init

--- with_setting/21278/1114.java
+++ with_setting/21278/1114.java
@@ -4,7 +4,16 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static int solution(int[][] office, int r, int c, String[] move) {
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] office = {{5,-1,4},{6,3,-1},{2,-1,1}};
+        int r = 1;
+        int c = 0;
+        String[] move = {"go","go","right","go","right","go","left","go"};
+        
+        System.out.println(solution(office, r, c, move)); // to be 14
     }
 }
+
