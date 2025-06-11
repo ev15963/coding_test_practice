f91bd32ee1e82e9655a74b84e806b39c5a3e80a6
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-11 19:20:30 +0900

    261.java test case init

--- with_setting/21278/261.java
+++ with_setting/21278/261.java
@@ -4,7 +4,13 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static long solution(int r, int c) {
+        return 0;
+    }
+
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(solution(2, 4)); // to be 4
+        // System.out.println(solution(3, 3)); // to be 6
     }
 }
