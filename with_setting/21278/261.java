f1471a9319017840b047eccf53f90ae36adc00a4
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-11 19:44:42 +0900

    261.java dp done

--- with_setting/21278/261.java
+++ with_setting/21278/261.java
@@ -5,7 +5,23 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     public static long solution(int r, int c) {
-        return 0;
+        int[][] dp = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i < c; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[r-1][c-1];
     }
 
     
