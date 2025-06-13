87162a0721875eecf83a94f37a683fcae813ef15
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-13 16:09:47 +0900

    329.java test case init

--- with_setting/21278/329.java
+++ with_setting/21278/329.java
@@ -4,7 +4,18 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static int solution(int n, int k) {
+        return -1;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n1 = 2;
+        int k1 = 2;
+
+        int n2 = 3;
+        int k2 = 2;
+        
+        System.out.println(solution(n1, k1)); // to be 2
+        System.out.println(solution(n2, k2)); // to be 18
     }
 }
