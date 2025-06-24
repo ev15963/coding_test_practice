67f1268fda747e7316bee219298a15d7799ef940
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-24 16:42:17 +0900

    689.java test case init

--- with_setting/21278/689.java
+++ with_setting/21278/689.java
@@ -4,7 +4,18 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    public static int solution(int[][] board, int c) {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] board1 = {{0,0,0,0,2,0,0,0,0,0},{0,0,1,1,1,1,1,0,0,0},{0,0,1,1,1,1,1,1,0,0},{0,0,1,1,1,1,1,0,1,0},{0,0,1,1,1,1,1,0,0,0},{0,0,0,0,3,0,0,0,1,0}};
+        int c1 = 1;
+
+        int[][] board2 = {{0,0,0,0,2,0,0,0,0,0},{0,0,1,1,1,1,1,0,0,0},{0,0,1,1,1,1,1,1,0,0},{0,0,1,1,1,1,1,0,1,0},{0,0,1,1,1,1,1,0,0,0},{0,0,0,0,3,0,0,0,1,0}};
+        int c2 = 2;
+            
+        System.out.println(solution(board1, c1));
+        System.out.println(solution(board2, c2));
     }
 }
