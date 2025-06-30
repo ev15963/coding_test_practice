a2fa8af94543a3e52c0294ec7c9e96968c62802c
nicky <62686044+ev15963@users.noreply.github.com>
2025-06-30 16:04:39 +0900

    10639.java 특수 기호에 따라 방향 이동

--- with_setting/185880/10639.java
+++ with_setting/185880/10639.java
@@ -5,7 +5,27 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     public static int solution(String[] drum) {
+        int leng = drum.length;
+        int dr = 0;
+        int dc = 0;
+        boolean checked = false;
+        int count = 0;
 
+        // 특수 기호에 따라 방향 이동
+        for (int i = 0; i < leng; i++) {
+            char[] strs = drum[i].toCharArray();
+                if (strs[dr] == '#') {
+                    dc += 1;
+                } else if (strs[dr] == '>') {
+                    dr += 1;
+                } else if (strs[dr] == '<') {
+                    dr -= 1;
+                } else if (strs[dr] == '*' && !checked) {
+                    checked = true;
+                    dc += 1;
+                }
+        }
+        
         return -1;
     }
     
